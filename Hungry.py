bf1c5596269ab0b05891ae64a1979bb0a4246b17
devendra-py <t.devendra121@gmail.com>
2018-09-10 17:58:30 +0530

    the commit message ... i am executed nested try exception practice

--- Hungry.py
+++ Hungry.py
@@ -1,7 +1,22 @@
-print("welcome")
+# working with nested try block exception :
 
-a=input("enter a status")
-if a=y:
-    print("eat samosa")
+try:
+   print("this is outer try block")
+   try:
+      print("this is inner try block")
+   except:
+
+       print("this is inner try block except")
+   else:
+        print("this is inner try block else block")
+
+   finally:
+        print("this is inner try block finnaly")
+
+except:
+   print("this is an outer except block")
 else:
-    print("practice u r programes")
+    print("this is else block of the outer block")
+finally:
+    print("this outer finally block")
+
